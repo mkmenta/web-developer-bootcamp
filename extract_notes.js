c806ac45ad5de249aea39837fa6d819cbaf26264
Mikel Menta Garde <mkmenta95@gmail.com>
2021-03-04 00:01:52 +0100

    Fix HTML tag parsing (#6)

--- extract_notes.js
+++ extract_notes.js
@@ -10,7 +10,7 @@ for (i = 0; i < notes.length; i++) {
   section = noteData.split('\n')[1];
   lecture = noteData.split('\n')[2];
   text = noteData.split(`${time}\n${section}\n${lecture}\n\n`)[1];
-  if (notesObject[section] == undefined){
+  if (notesObject[section] == undefined) {
     notesObject[section] = {};
   }
   if (notesObject[section][lecture] == undefined) {
@@ -22,12 +22,12 @@ for (i = 0; i < notes.length; i++) {
 // Export notes
 var output = "";
 var note;
-for ( let section in notesObject) {
+for (let section in notesObject) {
   output += `\n\n# ${section}\n`;
-  for ( let lecture in notesObject[section]) {
+  for (let lecture in notesObject[section]) {
     output += `\n## ${lecture}\n`;
-    for ( let note of notesObject[section][lecture]) {
-      output += `**(${note[0]}):** ${note[1]}\n\n`
+    for (let note of notesObject[section][lecture]) {
+      output += `**(${note[0]}):** ${note[1].replaceAll('<', '\\<').replaceAll('>', '\\>')}\n\n`
     }
   }
 }
